8f0bded03445c62716929d0d7b7bd6e6dcfb0f85
ManonDamoiseau <m.damoiseau05@hotmail.com>
2026-07-25 16:46:15 +0200

    Add domain documentation and detector status enum

--- RadiationMonitor.Domain/Enums/DetectorStatus.cs
+++ RadiationMonitor.Domain/Enums/DetectorStatus.cs
@@ -2,14 +2,11 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace RadiationMonitor.Domain.Enums
-{
-    internal class DetectorStatus // Les différents status du détecteur
-    {
-        public enum DetectorStatusEnum { //enum car un seul status possible à la fois
-            Online, 
-            Warning, 
-            Error
-        };
-    }
-}
+namespace RadiationMonitor.Domain.Enums;
+
+
+public enum DetectorStatusEnum { //enum car un seul status possible à la fois
+    Online, 
+    Warning, 
+    Error
+};
