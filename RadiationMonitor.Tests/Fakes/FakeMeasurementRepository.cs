eab28608780e361df6442c80d9cf9484c5db0bb3
ManonDamoiseau <m.damoiseau05@hotmail.com>
2026-08-06 17:40:42 +0200

    Start register measurement use case implementation

---
+++ RadiationMonitor.Tests/Fakes/FakeMeasurementRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RadiationMonitor.Application.Measurements.RegisterMeasurement;
+using RadiationMonitor.Domain.Entities;
+
+namespace RadiationMonitor.Tests.Fakes
+{
+    public class FakeMeasurementRepository : IMeasurementRepository
+    {
+        public List<Measurement> Measurements { get; } = new();
+
+        public void Add(Measurement measurement) 
+        {  
+            Measurements.Add(measurement); 
+        }
+    }
+}
