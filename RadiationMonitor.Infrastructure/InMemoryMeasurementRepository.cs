c44e77ff47f6db098e8d4d8bea928abb75222733
ManonDamoiseau <m.damoiseau05@hotmail.com>
2026-08-31 17:30:16 +0200

    Implement in-memory measurement repository

---
+++ RadiationMonitor.Infrastructure/InMemoryMeasurementRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RadiationMonitor.Application.Measurements.RegisterMeasurement;
+using RadiationMonitor.Domain.Entities;
+
+namespace RadiationMonitor.Infrastructure;
+
+public class InMemoryMeasurementRepository : IMeasurementRepository
+{
+    private readonly List<Measurement> _measurements = new();
+
+    public void Add(Measurement measurement)
+    {
+        _measurements.Add(measurement);
+    }
+}
