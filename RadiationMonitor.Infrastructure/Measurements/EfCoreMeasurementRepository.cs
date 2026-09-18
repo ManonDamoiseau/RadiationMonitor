648643bea1e578666bc036ea5506dc2312fc5bf1
ManonDamoiseau <m.damoiseau05@hotmail.com>
2026-09-18 16:52:50 +0200

    feat: Add SQL Server persistence foundation

---
+++ RadiationMonitor.Infrastructure/Measurements/EfCoreMeasurementRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RadiationMonitor.Application.Measurements.RegisterMeasurement;
+using RadiationMonitor.Domain.Entities;
+using RadiationMonitor.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RadiationMonitor.Infrastructure.Measurements
+{
+    public class EfCoreMeasurementRepository : IMeasurementRepository
+    {
+        private readonly RadiationMonitorDbContext _context;
+
+        public EfCoreMeasurementRepository(RadiationMonitorDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Measurement measurement)
+        {
+            _context.Measurements.Add(measurement);
+            _context.SaveChanges();
+        }
+
+        public Measurement? GetById(Guid id)
+        {
+            return _context.Measurements
+                .FirstOrDefault(measurement => measurement.Id == id);
+        }
+    }
+}
---
+++ RadiationMonitor.Infrastructure/Migrations/20260918133121_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RadiationMonitor.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Measurements",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DetectorId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Timestamp = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    DoseRate = table.Column<double>(type: "float", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Measurements", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Measurements");
+        }
+    }
+}
